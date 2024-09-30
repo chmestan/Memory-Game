4a8e760c67725a5e630daed7c20138a9d9aab2dd
Charlotte Mestan <cdmestan@outlook.com>
2024-09-30 16:22:29 +0200

    Small changes

--- Assets/#Project/Scripts/ChangeScene.cs
+++ Assets/#Project/Scripts/ChangeScene.cs
@@ -5,9 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    [SerializeField] float timeDelay=2f;
 
     IEnumerator LoadSceneAfterDelay(float waitTime, string name)
     {
+        waitTime = timeDelay;
         yield return new WaitForSeconds(waitTime);
         SceneManager.LoadScene(name);
         Debug.Log("Fin de la coroutine.");
